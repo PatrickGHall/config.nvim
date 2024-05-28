8b715b6faf45d97964da65e224063d00bf3b61b5
PatrickGHall <patrick.hall@flocksafety.com>
2024-05-28 12:45:49 -0400

    enable telescope and treesitter + add keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -26,6 +26,22 @@ set("n", "<M-.>", "<c-w>5>")
 set("n", "<M-t>", "<C-W>+")
 set("n", "<M-s>", "<C-W>-")
 
+-- Stolen from ThePrimeagen
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+set("x", "<leader>p", [["_dP]])
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
+set({ "n", "v" }, "<leader>d", [["_d]])
+set("i", "<C-c>", "<Esc>")
+set("n", "Q", "<nop>")
+set("n", "<leader>f", vim.lsp.buf.format)
+
 set("n", "<M-j>", function()
 	if vim.opt.diff:get() then
 		vim.cmd([[normal! ]c]])
